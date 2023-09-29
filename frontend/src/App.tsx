9354222fb92c2c60988d8042860e0fb349f900d1
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-09-29 23:07:31 +0530

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,9 +11,8 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
