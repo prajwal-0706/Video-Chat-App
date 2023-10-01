dba8a8d006e7390043a769714596ffe5f15c65c6
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-10-01 23:10:27 +0530

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,8 @@
-import viteLogo from '/vite.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <>App</>;
 }
 
 export default App;
