f69f479eacc84cd45758a4603a505a9a83ca5901
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-10-03 00:42:42 +0530

    Completed Backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
-
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Button variant="contained">Primary</Button>
+    </>
+  );
 }
 
 export default App;
