8196c037f7af38e98f8633d4b2dccb87a6b612b6
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-10-15 22:28:47 +0530

    Start of new Project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/**.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
